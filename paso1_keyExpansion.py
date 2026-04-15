564f1add7249390935060443f6057a34c77725bb
Abee <A01709167@tec.mx>
2026-04-14 19:20:59 -0600

    [Abi] key expansion: rot word

--- paso1_keyExpansion.py
+++ paso1_keyExpansion.py
@@ -5,6 +5,42 @@
 # Importar la llave
 from paso0_preprocesamiento import llaveEnBytes
 
-def keyExpansion(llave):
+llaveOriginal = llaveEnBytes
+def keyExpansion(llaveOriginal):
+    # Se deben conseguir 11 llaves
+    llaves = []
+    llaves.append(llaveOriginal)
+    
+    # 1. Dividir en 4 subllaves, con 4 pares
+    # Dividimos en pares
+    pares = []
+    for i in range(0, len(llaveOriginal), 2):
+        par = llaveOriginal[i:i+2]
+        pares.append(par)
 
-    return llave
+    # Creamos 4 palabras de 4 pares
+    palabras = []
+    for i in range(0, len(pares), 4):
+        palabra = pares[i:i+4]
+        palabras.append(palabra)
+    
+    print("Palabras: ", palabras)
+
+    # Necesitamos 4 palabras para cada ronda (11x4)
+
+    # Procesar palabras
+    rotWords = []
+
+    for palabra in palabras:
+        rw = rotWord(palabra)
+        rotWords.append(rw)
+        print("RotWord:", rw)
+
+
+    # ------------------------------------------------------------
+    return llaves
+
+def rotWord(palabra):
+    return palabra[1:] + palabra[:1]
+
+keyExpansion(llaveOriginal)
